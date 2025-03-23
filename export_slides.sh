8fb031345d32a0a3ef50c0c011526031a106d33f
Jacob Albrecht <6445835+chepyle@users.noreply.github.com>
2025-03-23 03:00:19 +0000

    clean up files

---
+++ export_slides.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+
+jupyter nbconvert MultiAgentDemoPresentation.ipynb --to slides --post serve --no-input --no-prompt
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/bash
 
-autogenstudio ui --port 8081 --appdir ./app
+export OPENROUTER_API_KEY="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"
+
+APP_DIR=${1:-./app} # Use the first argument as the directory or default to ./app
+autogenstudio ui --port 8081 --appdir "$APP_DIR"
