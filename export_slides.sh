70a28a1c9296cbdd5ef2494aeaed1b1faadd0922
Jacob Albrecht <6445835+chepyle@users.noreply.github.com>
2025-03-28 16:47:14 +0000

    first draft materials

--- export_slides.sh
+++ export_slides.sh
@@ -2,6 +2,7 @@
 
 # Export to slides
 jupyter nbconvert  MultiAgentDemoPresentation.ipynb --to slides --output-dir='./docs' --output='index' --no-input --no-prompt
+mv ./docs/index.slides.html ./docs/index.html
 
 # Export to PDF
 jupyter nbconvert MultiAgentDemoPresentation.ipynb --to pdf --no-input --no-prompt
